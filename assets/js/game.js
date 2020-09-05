2514b48cd04184a04367480bdd6386cb015a57d6
OspreyVelt <68624499+OspreyVelt@users.noreply.github.com>
2020-09-04 21:12:18 -0600

    initial JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("this is an alert! Javascript is running");
+var playerName = window.prompt("What is your robot's name?");
+//what is this?
+console.log(playerName);
+//what is this
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  // fight()
